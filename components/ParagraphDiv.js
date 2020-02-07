ce662827c29cb454c58dc6ef581603601d2f3ef8
Livingstone Asabahebwa <ahebwa49@gmail.com>
2020-02-07 15:34:07 +0300

    correct wording

--- components/ParagraphDiv.js
+++ components/ParagraphDiv.js
@@ -16,18 +16,12 @@ const ParagraphDiv = () => {
   return (
     <div className="paragraph" style={styles.container} id="frontParagraph">
       <p style={styles.p}>
-        I am a full stack developer and architect with 2+ years of
+        I am a full stack developer from the internet with over 2 years of
         commercial experience. I have consulted for a few clients, providing
         guidance on varied software projects. These days I work mainly with
-        react, redux or apollo-client for frontend, architecting solutions
-        around node, prisma, express, MongoDB and PostgreSQL at the back-end using
-        both GraphQL and REST API's.
-        <br />
-        <br />
-        As an architect I provide my clients with advice and best practice
-        recommendations in areas including: source code optimization, unit
-        testing and continuous deployment. I have provided software engineering
-        practice advice for projects written in only JavaScript.
+        react, react native, redux or apollo-client and D3 for frontend,
+        architecting solutions around node, express, and MongoDB at the back-end
+        using both GraphQL and REST API's.
         <br />
         <br />I am currently located in Kampala, Uganda and work with clients
         from anywhere in the World.
--- components/ProfileParagraph.js
+++ components/ProfileParagraph.js
@@ -13,11 +13,11 @@ const ProfileParagraph = () => {
   return (
     <div className="paragraph" style={styles.paragraph} id="profileParagraph">
       <p>
-        Hello, Night Owl. I’m Livingstone, a software developer from the Uganda
-        and I design and build apps for devices, big and small. I have a
-        hardwired desire to be better, an instinct for good design, a strong
-        ambition to make a difference on the web, embarrassing hang-ups on
-        semantics, and evangelistic tendencies.
+        Hello, Night Owl. I’m Livingstone, a software developer from the
+        internet building apps for devices, big and small. I have a hardwired
+        desire to be better, an instinct for good design, a strong ambition to
+        make a difference on the web, embarrassing hang-ups on semantics, and
+        evangelistic tendencies.
         <br />
         Look me up on: <FrontSocialParagraph />
       </p>
